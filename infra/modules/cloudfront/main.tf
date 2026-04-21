550b1c4fd03c904a1998658271dee30a02fb9d1b
kenzathr <kanza.tahri.joutey@gmail.com>
2026-04-21 16:01:21 +0100

    Add CloudFront key group and public key resources

--- infra/modules/cloudfront/main.tf
+++ infra/modules/cloudfront/main.tf
@@ -1,29 +1,47 @@
+resource "aws_cloudfront_key_group" "pfe_key_group" {
+  name    = "axeon-key-group"
+  items   = [aws_cloudfront_public_key.pfe_key.id]
+}
+
+resource "aws_cloudfront_public_key" "pfe_key" {
+  name        = "${var.project_name}-public-key"
+  encoded_key = var.public_key_pem
+}
+
 resource "aws_cloudfront_distribution" "cdn" {
   enabled = true
 
   origin {
-    domain_name = var.bucket_regional_domain_name
-    origin_id   = "S3Origin"
-    s3_origin_config {
-      origin_access_identity = "" 
-    }
+    domain_name              = "kanza-file-api-files-dev.s3.eu-west-3.amazonaws.com"
+    origin_id                = "S3Origin"
+    origin_access_control_id = "E2452J5OTLZ5CR" # Ton OAC réel
   }
 
   default_cache_behavior {
     target_origin_id       = "S3Origin"
     viewer_protocol_policy = "redirect-to-https"
     
-    # INDISPENSABLE pour la validation
-    allowed_methods  = ["GET", "HEAD", "OPTIONS"]
-    cached_methods   = ["GET", "HEAD"]
+    allowed_methods = ["GET", "HEAD", "OPTIONS"]
+    cached_methods  = ["GET", "HEAD"]
     
+    # Utilisation du groupe de clés existant
     trusted_key_groups = [aws_cloudfront_key_group.pfe_key_group.id]
 
     forwarded_values {
       query_string = true
-      cookies { forward = "none" }
+      cookies {
+        forward = "none"
+      }
     }
   }
-  
-  # ... (conserve tes blocs restrictions et viewer_certificate)
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
 }
