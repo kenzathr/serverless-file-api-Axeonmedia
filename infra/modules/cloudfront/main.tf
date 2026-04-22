2dab343a145cdb4c7e40459f4fc75058df040596
kenzathr <kanza.tahri.joutey@gmail.com>
2026-04-22 18:13:08 +0100

    Refactor CloudFront configuration with variables

--- infra/modules/cloudfront/main.tf
+++ infra/modules/cloudfront/main.tf
@@ -1,10 +1,10 @@
 resource "aws_cloudfront_key_group" "pfe_key_group" {
-  name    = "axeon-key-group"
-  items   = [aws_cloudfront_public_key.pfe_key.id]
+  name = "axeon-key-group"
+  items = [aws_cloudfront_public_key.pfe_key.id]
 }
 
 resource "aws_cloudfront_public_key" "pfe_key" {
-  name        = "${var.project_name}-public-key"
+  name = "${var.project_name}-public-key"
   encoded_key = var.public_key_pem
 }
 
@@ -12,23 +12,23 @@ resource "aws_cloudfront_distribution" "cdn" {
   enabled = true
 
   origin {
-    domain_name              = "kanza-file-api-files-dev.s3.eu-west-3.amazonaws.com"
-    origin_id                = "S3Origin"
-    origin_access_control_id = "E2452J5OTLZ5CR" # Ton OAC réel
+    domain_name = var.bucket_regional_domain_name
+    origin_id   = "S3Origin"
+    origin_access_control_id = var.origin_access_control_id
   }
 
   default_cache_behavior {
     target_origin_id       = "S3Origin"
     viewer_protocol_policy = "redirect-to-https"
-    
-    allowed_methods = ["GET", "HEAD", "OPTIONS"]
-    cached_methods  = ["GET", "HEAD"]
-    
+    allowed_methods        = ["GET", "HEAD", "OPTIONS"]
+    cached_methods         = ["GET", "HEAD"]
+
     # Utilisation du groupe de clés existant
     trusted_key_groups = [aws_cloudfront_key_group.pfe_key_group.id]
 
     forwarded_values {
       query_string = true
+
       cookies {
         forward = "none"
       }
