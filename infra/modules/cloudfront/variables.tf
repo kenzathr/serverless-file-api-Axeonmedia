c5a3955932655edace50cfdc80314c7de9ecb0f5
kenzathr <kanza.tahri.joutey@gmail.com>
2026-04-22 18:39:01 +0100

    Add missing S3 bucket and CloudFront variables
    Added missing variables for S3 bucket and CloudFront.

--- infra/modules/cloudfront/variables.tf
+++ infra/modules/cloudfront/variables.tf
@@ -1,22 +1,36 @@
-variable "aws_region" {
-  description = "Region AWS de deploiement"
-  type        = string
-  default     = "eu-west-3"
-}
-
 variable "project_name" {
-  description = "Prefixe utilise pour nommer toutes les ressources"
+  description = "Nom du projet"
   type        = string
-  default     = "kanza-file-api"
 }
 
 variable "environment" {
-  description = "Environnement de deploiement"
+  description = "Environnement (dev, prod...)"
   type        = string
-  default     = "dev"
 }
 
 variable "public_key_pem" {
-  description = "Contenu de la cle publique pour CloudFront"
+  description = "Clé publique pour la signature CloudFront"
+  type        = string
+}
+
+# --- LES VARIABLES MANQUANTES À AJOUTER ---
+
+variable "bucket_id" {
+  description = "ID du bucket S3"
+  type        = string
+}
+
+variable "bucket_arn" {
+  description = "ARN du bucket S3"
+  type        = string
+}
+
+variable "bucket_regional_domain_name" {
+  description = "Nom de domaine régional du bucket S3"
+  type        = string
+}
+
+variable "origin_access_control_id" {
+  description = "ID de l'OAC CloudFront"
   type        = string
 }
