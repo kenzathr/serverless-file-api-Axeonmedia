a535339205c8f6628a6728c58be388cfd2ba5f05
kenzathr <kanza.tahri.joutey@gmail.com>
2026-04-25 17:59:29 +0100

    Change default AWS region to us-east-1

--- infra/variables.tf
+++ infra/variables.tf
@@ -1,7 +1,7 @@
 variable "aws_region" {
   description = "Region AWS de deploiement"
   type        = string
-  default     = "eu-west-3"
+  default     = "us-east-1" 
 }
 
 variable "project_name" {
