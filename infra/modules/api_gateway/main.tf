885d9aa303979f6330d4e6e41f5171d05020716e
kenzathr <kanza.tahri.joutey@gmail.com>
2026-05-07 15:14:03 +0100

    Update cognito authorizer to use variable ARN

--- infra/modules/api_gateway/main.tf
+++ infra/modules/api_gateway/main.tf
@@ -34,7 +34,7 @@ resource "aws_api_gateway_authorizer" "cognito" {
   name                   = "cognito-authorizer"
   rest_api_id            = aws_api_gateway_rest_api.api.id
   type                   = "COGNITO_USER_POOLS"
-  provider_arns          = [aws_cognito_user_pool.axeon_user_pool.arn]
+  provider_arns = [var.cognito_user_pool_arn]
   identity_source        = "method.request.header.Authorization"
 }
 
